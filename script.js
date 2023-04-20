b543dffff73ba12c86dbd6e06e6ba857211cb50f
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-20 13:41:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name = name;
+	this.age = age;
+}
+Person.prototype.greet = ()=>{
+	return `Hello, my name is ${this.name}, I am ${this.age} years old`;
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	this.name = name;
+	this.age = age;
+	this.jobTitle = jobTitle;
+
+	this.jobGreet = ()=>{
+		return this.greet() + `, and my job title is ${this.jobTitle}`
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
